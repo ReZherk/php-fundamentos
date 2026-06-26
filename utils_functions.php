e16abcd312adb43328f4a263df2ae8203d0e2a0b
ReZherk <patrickcomeresbueno@gmail.com>
2026-06-26 01:17:08 -0500

    add the utils_functions file

---
+++ utils_functions.php
@@ -0,0 +1,24 @@
+<?php
+$nombre="ZOE";
+$numeros=[1,2,3];
+
+
+//---------Para imprimir en consola-----------
+echo $nombre;
+
+echo "\n"; //Salto de linea
+
+
+print_r($numeros); //Este muestra el contenido  de lo que esta dentro de forma legible.
+
+var_dump($numeros); //Este muestra el tipo de lo que esta dentro y tambien el tipo de lo que esta dentro de este.
+
+
+
+
+/* $miNombre="Patrick";
+$frutas=["manzana","banana","keke"];
+
+print_r("hola mi nombre es ".$miNombre." y las frutas que tengo son: ");
+print_r($frutas);
+ */
